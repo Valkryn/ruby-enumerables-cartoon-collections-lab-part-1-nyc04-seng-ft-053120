bbfc65bf747458d00014f92c5438f5a6d2151f93
Calvin Gutierrez <calvinraphaelg@gmail.com>
2020-06-05 19:28:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,5 @@
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
-end
+ array.each
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
